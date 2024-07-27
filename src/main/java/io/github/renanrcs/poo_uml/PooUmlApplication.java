33442152641598156d2a9ab55438fb57c94aea90
renanrcs <renan.cavalcante@yahoo.com>
2024-07-27 10:19:00 -0300

    First commit

---
+++ src/main/java/io/github/renanrcs/poo_uml/PooUmlApplication.java
@@ -0,0 +1,13 @@
+package io.github.renanrcs.poo_uml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooUmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PooUmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/renanrcs/poo_uml/PooUmlApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.renanrcs.poo_uml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PooUmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
